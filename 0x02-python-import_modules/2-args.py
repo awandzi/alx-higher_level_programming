31805d373d835b83b5991cc3816bbffa6716df95
awandzi <kwamenafo@gmail.com>
2024-01-04 17:20:17 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+   from  import sys import argv
+
+    a = len(argv) - 1
+    if a == 1:
+        print("{} arguments.".format(a))
+    elif a == 1:
+        print("{} argument:".format(a))
+    else:
+        print("{} arguments:".format(a))
+    for i in range(a):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
+
