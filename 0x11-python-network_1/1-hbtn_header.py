8f138c06ef46acdb9fd7d814fdd70c038b9a4381
awandzi <kwamenafo@gmail.com>
2024-04-26 16:16:37 -0700

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-with urllib.request.urlopen(url) as response:
-            request_Id = response.headers.get('X-Request-Id')
+    with urllib.request.urlopen(url) as response:
+            headers = response.headers
+            request_id = headers.get('X-Request-Id')
